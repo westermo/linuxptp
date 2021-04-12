0e2632ec8f060ab9891da7c11e18c162fbbc88ec
Anders Öhlander <anders.ohlander@westermo.se>
2021-04-12 15:15:25 +0200

    Add vlan support for the power profile
    Support transmitting an egress vlan id and priority as required by the
    power profile. With this comes three new config options:
    
    	egress_vlan.tagged # Enables the others
    	egress_vlan.id
    	egress_vlan.prio
    
    Signed-off-by: Anders Öhlander <aggelander@gmail.com>
    Signed-off-by: Patrik Lundgren <patrik.lundgren@outlook.com>

--- config.c
+++ config.c
@@ -339,6 +339,10 @@ struct config_item config_tab[] = {
 	GLOB_ITEM_INT("utc_offset", CURRENT_UTC_OFFSET, 0, INT_MAX),
 	GLOB_ITEM_INT("verbose", 0, 0, 1),
 	GLOB_ITEM_INT("write_phase_mode", 0, 0, 1),
+
+	GLOB_ITEM_INT("egress_vlan.tagged", 0, 0, 1),
+	GLOB_ITEM_INT("egress_vlan.id", 0, 0, 4095),
+	GLOB_ITEM_INT("egress_vlan.prio", 0, 0, 7),
 };
 
 static struct unicast_master_table *current_uc_mtab;
