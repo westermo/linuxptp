34febd1eccd784fbe225d5e2d5ca039245c076ef
Casper Andersson <casper.casan@gmail.com>
2024-12-18 20:17:22 +0100

    ts2phc: Treat PASSIVE_SLAVE as UNCALIBRATED when no other ref clk is available
    This is to avoid the case of ts2phc falling back to selecting the last
    clock in the list as refclk. If the previous UNCALIBRATED/SLAVE port
    changed state (lost link or master) it may briefly end up in a state
    where the PASSIVE_SLAVE hasn't yet transitioned to UNCALIBRATED.
    
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- ts2phc.c
+++ ts2phc.c
@@ -324,7 +324,7 @@ static int ts2phc_auto_init_ports(struct ts2phc_private *priv)
 
 static void ts2phc_reconfigure(struct ts2phc_private *priv)
 {
-	struct ts2phc_clock *c, *ref_clk = NULL, *last = NULL;
+	struct ts2phc_clock *c, *ref_clk = NULL, *last = NULL, *passive_slave = NULL;
 	int num_ref_clocks = 0, num_target_clocks = 0;
 
 	pr_info("reconfiguring after port state change");
@@ -337,6 +337,9 @@ static void ts2phc_reconfigure(struct ts2phc_private *priv)
 		}
 
 		switch (c->state) {
+		case PS_PASSIVE_SLAVE:
+			passive_slave = c;
+			/* fallthrough */
 		case PS_FAULTY:
 		case PS_DISABLED:
 		case PS_LISTENING:
@@ -366,7 +369,14 @@ static void ts2phc_reconfigure(struct ts2phc_private *priv)
 		}
 		last = c;
 	}
-	if (num_target_clocks >= 1 && !ref_clk) {
+
+	if (passive_slave && !ref_clk) {
+		/* Treat PASSIVE_SLAVE same as UNCALIBRATED, i.e. wait for clock to be ready */
+		num_ref_clocks++;
+		num_target_clocks--;
+	}
+
+	if (num_target_clocks >= 1 && !ref_clk && num_ref_clocks == 0) {
 		priv->ref_clock = last;
 		priv->ref_clock->is_target = false;
 		/* Reset to original state in next reconfiguration. */
